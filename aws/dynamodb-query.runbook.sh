563e3df0f7e9b9155e4b61912c170b6ce63c0a52
bharadwaja-liveramp <bharadwaja.vissavajhula@liveramp.com>
2025-06-04 17:14:35 +0530

    Update dynamodb-query.runbook.sh to add region selection

--- aws/dynamodb-query.runbook.sh
+++ aws/dynamodb-query.runbook.sh
@@ -5,11 +5,13 @@ TABLE_NAME='{{ .tableName | type "select" | description "the name of the table" 
 KEY_CONDITION_EXPR='{{ .keyExpression | description "The condition that specifies the key values for items to beretrieved by the Query action" | required "keyExpression is required" }}'
 EXPR_ATTRIBUTE_NAMES='{{ .exprAttributeNames | description "One or more substitution tokens for attribute names in an expression" | required "exprAttributeNames is required" }}'
 EXPR_ATTRIBUTE_VALUES='{{ .exprAttributeValues | description " One or more values that can be substituted in an expression" | required "exprAttributeValues is required" }}'
+REGION_NAME='{{ .regionName | type "select" | description "Select a region" | options "eu-west-1" "us-east-1" }}'
 
 aws dynamodb query \
     --no-cli-pager \
     --no-cli-auto-prompt \
     --table-name $TABLE_NAME \
+    --region $REGION_NAME \
     --key-condition-expression "$KEY_CONDITION_EXPR" \
     --expression-attribute-names "$EXPR_ATTRIBUTE_NAMES" \
     --expression-attribute-values "$EXPR_ATTRIBUTE_VALUES"
