40bfb607c1eb6d596fd2e1d333de116e7bec971f
bharadwaja-liveramp <bharadwaja.vissavajhula@liveramp.com>
2025-06-19 11:17:38 +0530

    Update dynamodb-scan.runbook.sh revert table

--- aws/dynamodb-scan.runbook.sh
+++ aws/dynamodb-scan.runbook.sh
@@ -7,6 +7,7 @@ FILTER_TYPE='{{ .filterType | type "select" | description "Type" | options "Stri
 FILTER_CONDITION='{{ .filterCondition | type "select" | description "Condition" | options "Equal_to" "Not_equal_to" "Less_than_or_equal_to" "Less_than" "Greater_than_or_equal_to" "Greater_than" "Between" "Exists" "Not_exists" "Contains" "Not_contains" "Begins_with" | default "Equal_to" }}'
 FILTER_ATTRIBUTE='{{ .filterAttribute | description "Attribute name" }}'
 
+TABLE_NAME='{{ .tableName | type "select" | description "the name of the table" | options "ats-placement-versions-dev" "ats-direct-v2-configs-dev" "user-details-dev" "launchpadVersions-dev" "multiAccounts-dev" }}'
 
 # -------- Build and Run Command --------
 CMD="aws dynamodb scan --table-name \"$TABLE_NAME\" --region \"$REGION_NAME\" "
