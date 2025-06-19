38e0b010783b227d91f10138bd3093782a60e0b0
bharadwaja-liveramp <bharadwaja.vissavajhula@liveramp.com>
2025-06-19 11:14:18 +0530

    Update dynamodb-scan.runbook.sh to remove table selection

--- aws/dynamodb-scan.runbook.sh
+++ aws/dynamodb-scan.runbook.sh
@@ -6,7 +6,6 @@ FILTER_VALUE='{{ .filterValue | description "Enter attribute value" }}'
 FILTER_TYPE='{{ .filterType | type "select" | description "Type" | options "String" "Number" "Binary" "Boolean" "Null" | default "String" }}'
 FILTER_CONDITION='{{ .filterCondition | type "select" | description "Condition" | options "Equal_to" "Not_equal_to" "Less_than_or_equal_to" "Less_than" "Greater_than_or_equal_to" "Greater_than" "Between" "Exists" "Not_exists" "Contains" "Not_contains" "Begins_with" | default "Equal_to" }}'
 FILTER_ATTRIBUTE='{{ .filterAttribute | description "Attribute name" }}'
-TABLE_NAME='{{ .tableName | type "select" | description "the name of the table" | options "ats-placement-versions-dev" "ats-direct-v2-configs-dev" "user-details-dev" "launchpadVersions-dev" "multiAccounts-dev" }}'
 
 
 # -------- Build and Run Command --------
