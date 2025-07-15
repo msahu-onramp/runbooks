33ac781d7a88b03b99b80fb7aa5c7e87ba920ff8
bharadwaja-liveramp <bharadwaja.vissavajhula@liveramp.com>
2025-07-15 22:00:39 +0530

    Update dynamodb-query.runbook.sh

--- aws/dynamodb-query.runbook.sh
+++ aws/dynamodb-query.runbook.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -eo pipefail
 
-TABLE_NAME='{{ .tableName | type "select" | description "the name of the table" | options "ats-placement-versions-dev" "ats-direct-v2-configs-dev" "user-details-dev" "launchpadVersions-dev" "multiAccounts-dev" }}'
 KEY_CONDITION_EXPR='{{ .keyExpression | description "The condition that specifies the key values for items to beretrieved by the Query action" | required "keyExpression is required" }}'
 EXPR_ATTRIBUTE_NAMES='{{ .exprAttributeNames | description "One or more substitution tokens for attribute names in an expression" | required "exprAttributeNames is required" }}'
 EXPR_ATTRIBUTE_VALUES='{{ .exprAttributeValues | description " One or more values that can be substituted in an expression" | required "exprAttributeValues is required" }}'
-REGION_NAME='{{ .regionName | type "select" | description "Select a region" | options "eu-west-1" "us-east-1" }}'
 
 aws dynamodb query \
     --no-cli-pager \
     --no-cli-auto-prompt \
     --table-name $TABLE_NAME \
-    --region $REGION_NAME \
     --key-condition-expression "$KEY_CONDITION_EXPR" \
     --expression-attribute-names "$EXPR_ATTRIBUTE_NAMES" \
     --expression-attribute-values "$EXPR_ATTRIBUTE_VALUES"
+
+
+# REGION_NAME='{{ .regionName | type "select" | description "Select a region" | options "eu-west-1" "us-east-1" }}'
+# TABLE_NAME='{{ .tableName | type "select" | description "the name of the table" | options "ats-placement-versions-dev" "ats-direct-v2-configs-dev" "user-details-dev" "launchpadVersions-dev" "multiAccounts-dev" }}'
